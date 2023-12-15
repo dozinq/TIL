315ed9719719e276a30d1e41749d3d3a546d36a4
dojinuk <dojinuk@kakao.com>
2023-12-15 23:34:48 +0900

    Create 03_declaring_variable.js

---
+++ 10_CodeFactory_JS_Full/01_basics/03_declaring_variable.js
@@ -0,0 +1,46 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ */
+
+var name = 'CodeFactory';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = 'fromis';
+console.log(ive)
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = 'STAYC';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당 -> 하지 않을 경우 undefined 반환 (const는 선언과 동시에 할당하여야 한다.)
+ */
+var exname;
+exname = 'CF';
+console.log(exname)
+
+let gF;
+console.log(gF);
+
+// const gF2;
+
+
