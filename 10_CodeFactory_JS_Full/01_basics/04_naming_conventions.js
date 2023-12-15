87ec45e510280f00f764f46395c40bf8187f7640
dojinuk <dojinuk@kakao.com>
2023-12-15 23:41:27 +0900

    Create 04_naming_conventions.js

---
+++ 10_CodeFactory_JS_Full/01_basics/04_naming_conventions.js
@@ -0,0 +1,30 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어(라틴문자)를 사용하며 문자와 숫자를 모두 사용할 수 있다.
+ * 2) 특수기호는 _와 $를 사용할 수 있다.
+ * 3) 숫자로 이름을 시작할 수 없다.
+ * 4) 키워드는 변수명으로 사용할 수 없다.
+ */
+
+let codeFactory = '코드팩토리';
+var $ive = '아이브';
+const _ive = '아이브2';
+
+console.log(codeFactory);
+console.log($ive);
+console.log(_ive);
+
+// let 1name = 'no';
+// let const = 'attention';
+
+/**
+ * Naming Convention 2
+ * 
+ * 1) camelCase - 대부분의 언어에서 많이 사용
+ * 2) snake_case - Python에서 사용
+ * 3) PascalCase - C# MS 계열의 언어에서 사용
+ */
+
+const doJinUk = '도진욱';
+console.log(doJinUk);
