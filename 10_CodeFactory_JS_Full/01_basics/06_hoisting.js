18f04576217890b213ac9fbfbb900eaebe1f6829
dojinuk <dojinuk@kakao.com>
2023-12-16 23:53:53 +0900

    Create 06_hoisting.js

---
+++ 10_CodeFactory_JS_Full/01_basics/06_hoisting.js
@@ -0,0 +1,15 @@
+/**
+ * Hoisting
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ */
+console.log('Hello');
+console.log('World');
+
+console.log(name);
+var name = '진욱';
+console.log(name);
+
+console.log(jinUk);
+let jinUk = '진욱';
+const jinUk2 = '진욱';
