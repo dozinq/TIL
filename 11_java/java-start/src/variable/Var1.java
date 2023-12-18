7f693048bead4c3196d025ac8781d52c19a473f9
dojinuk <dojinuk@kakao.com>
2023-12-18 21:04:12 +0900

    Create Var1.java

---
+++ 11_java/java-start/src/variable/Var1.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
