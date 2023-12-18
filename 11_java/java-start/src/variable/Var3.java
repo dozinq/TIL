19c85b632d27d63fcae36044be26de58d4bf5d26
dojinuk <dojinuk@kakao.com>
2023-12-18 23:06:25 +0900

    Create Var3.java

---
+++ 11_java/java-start/src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 10; // 변수 초기화
+        System.out.println(a);
+        a = 50;
+        System.out.println(a);
+    }
+}
