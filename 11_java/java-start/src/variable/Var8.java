4b3132f07d29b5fb7bbf056107445d55254fe4a9
dojinuk <dojinuk@kakao.com>
2023-12-19 10:02:33 +0900

    Create Var8.java

---
+++ 11_java/java-start/src/variable/Var8.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var8 {
+
+    public static void main(String[] args) {
+        // 정수
+//        byte b = 127; // -128 ~ 127
+//        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // -2,147,483,648 ~ 2,147,483,647 (약 21억)
+        long l = 9223372036854775807L;
+
+        // 실수
+//        float f = 10.0f;
+        double d = 10.0;
+    }
+}
