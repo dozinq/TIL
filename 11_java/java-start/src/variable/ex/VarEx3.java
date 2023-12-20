2830bb80876b4fefd51abe35ccfd0efaf8660b5c
dojinuk <dojinuk@kakao.com>
2023-12-20 10:25:08 +0900

    Create VarEx3.java

---
+++ 11_java/java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long var1 = 10000000000L;
+        System.out.println(var1);
+        boolean var2 = true;
+        System.out.println(var2);
+    }
+}
