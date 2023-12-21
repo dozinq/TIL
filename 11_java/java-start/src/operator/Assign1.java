289111461b8d77a6d74c50655708add8324e857e
dojinuk <dojinuk@kakao.com>
2023-12-21 10:10:49 +0900

    Create Assign1.java

---
+++ 11_java/java-start/src/operator/Assign1.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class Assign1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        a += 3;
+        a -= 2;
+        a *= 4;
+        a /= 3;
+        a %= 5;
+        System.out.println(a);
+    }
+}
