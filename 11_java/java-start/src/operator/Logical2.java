0892e8cc676b928e526f9ca69a817620241d23a5
dojinuk <dojinuk@kakao.com>
2023-12-22 12:21:10 +0900

    Create Logical2.java

---
+++ 11_java/java-start/src/operator/Logical2.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Logical2 {
+
+    public static void main(String[] args) {
+        int a = 15;
+        // a는 10보다 크고 20보다 작다
+        boolean result = a > 10 && a < 20;
+        System.out.println("result = " + result);
+    }
+}
