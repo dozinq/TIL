274581ace039857a468c41b15cd65620850a870c
dojinuk <dojinuk@kakao.com>
2023-12-23 11:28:18 +0900

    Create OperationEx1.java

---
+++ 11_java/java-start/src/operator/ex/OperationEx1.java
@@ -0,0 +1,12 @@
+package operator.ex;
+
+public class OperationEx1 {
+
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, num3 = 30;
+        int sum = num1 + num2 + num3;
+        int average = sum / 3;
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+    }
+}
