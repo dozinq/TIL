d7767efb9ba1f8c5edb56ca450e8020cf73d77f5
dojinuk <dojinuk@kakao.com>
2023-12-29 09:28:54 +0900

    Create CondEx6.java

---
+++ 11_java/java-start/src/cond/ex/CondEx6.java
@@ -0,0 +1,11 @@
+package cond.ex;
+
+public class CondEx6 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int answer = (a > b) ? a : b;
+
+        System.out.println("더 큰 숫자는 " + answer + "입니다.");
+    }
+}
