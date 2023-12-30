44ec76a119ef13a99bb8a142c92a98467e5e9224
dojinuk <dojinuk@kakao.com>
2023-12-30 10:20:30 +0900

    Create Casting2.java

---
+++ 11_java/java-start/src/casting/Casting2.java
@@ -0,0 +1,12 @@
+package casting;
+
+public class Casting2 {
+
+    public static void main(String[] args) {
+        double doubleValue = 1.5;
+        int intValue = 0;
+
+        intValue = (int) doubleValue;
+        System.out.println(intValue);
+    }
+}
