64d44f14b320050cdb46daf716772c0d1cd6fd12
dojinuk <dojinuk@kakao.com>
2024-01-03 00:08:33 +0900

    Create ForEx1.java

---
+++ 11_java/java-start/src/loop/ex/ForEx2.java
@@ -0,0 +1,10 @@
+package loop.ex;
+
+public class ForEx2 {
+
+    public static void main(String[] args) {
+        for (int num = 2; num <= 20; num += 2) {
+            System.out.println(num);
+        }
+    }
+}
