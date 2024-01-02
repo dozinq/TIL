96f279d2add52aee3d014e34aea4a77965d2813b
dojinuk <dojinuk@kakao.com>
2024-01-03 00:08:39 +0900

    Create ForEx1.java

---
+++ 11_java/java-start/src/loop/ex/ForEx3.java
@@ -0,0 +1,12 @@
+package loop.ex;
+
+public class ForEx3 {
+
+    public static void main(String[] args) {
+        int max = 100, sum = 0;
+        for (int i = 1; i <= max; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
