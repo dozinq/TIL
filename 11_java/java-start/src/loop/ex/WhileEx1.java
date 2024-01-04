56691050dfff040c01fe7c9eaee748ecf975a505
dojinuk <dojinuk@kakao.com>
2024-01-05 05:25:15 +0900

    Create WhileEx1.java

---
+++ 11_java/java-start/src/loop/ex/WhileEx1.java
@@ -0,0 +1,13 @@
+package loop.ex;
+
+public class WhileEx1 {
+
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 10) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
