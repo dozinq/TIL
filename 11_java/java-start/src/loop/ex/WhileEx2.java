088995b743bcf0f9148da2d4fc0df244664e3bcf
dojinuk <dojinuk@kakao.com>
2024-01-05 07:02:15 +0900

    Create WhileEx2.java

---
+++ 11_java/java-start/src/loop/ex/WhileEx2.java
@@ -0,0 +1,13 @@
+package loop.ex;
+
+public class WhileEx2 {
+
+    public static void main(String[] args) {
+        int num = 2;
+
+        while (num <= 20) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
