0a96febe5e196f375c987fed52b2c09993b8d15b
dojinuk <dojinuk@kakao.com>
2024-01-06 10:58:53 +0900

    Create ScannerEx3.java

---
+++ 11_java/java-start/src/scanner/ex/ScannerEx3.java
@@ -0,0 +1,23 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerEx3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("음식 이름을 입력해주세요: ");
+        String foodName = scanner.nextLine();
+
+        System.out.print("음식의 가격을 입력해주세요: ");
+        int foodPrice = scanner.nextInt();
+
+        System.out.print("음식의 수량을 입력해주세요: ");
+        int foodQuantity = scanner.nextInt();
+
+        int totalPrice = foodPrice * foodQuantity;
+
+        System.out.println(foodName + " " + foodQuantity + "개를 주문하셨습니다. 총 가격은 " + totalPrice + "원입니다.");
+    }
+}
