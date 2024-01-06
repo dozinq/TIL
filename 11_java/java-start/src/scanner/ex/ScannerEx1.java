1c9506cdd0eada0aff0523302184c2fb0231e3ee
dojinuk <dojinuk@kakao.com>
2024-01-06 10:58:44 +0900

    Create ScannerEx1.java

---
+++ 11_java/java-start/src/scanner/ex/ScannerEx1.java
@@ -0,0 +1,20 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("당신의 이름을 입력하세요: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("당신의 나이를 입력하세요: ");
+        int userAge = scanner.nextInt();
+
+        System.out.println("당신의 이름은 " + userName + "이고, 나이는 " + userAge + "살입니다.");
+
+
+    }
+}
