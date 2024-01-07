68041c3b7b38523eb6067751270a4724f659feee
dojinuk <dojinuk@kakao.com>
2024-01-07 16:46:54 +0900

    Create ScannerEx5.java

---
+++ 11_java/java-start/src/scanner/ex/ScannerEx5.java
@@ -0,0 +1,18 @@
+package scanner.ex;
+
+public class ScannerEx5 {
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int temp;
+        
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        
+    }
+}
